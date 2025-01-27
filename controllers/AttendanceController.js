217fc26a0f6a52aadea99396b1aaad12565b0567
Sanjay-adepu <adepusanjay812@gmail.com>
2025-01-27 23:35:17 +0530

    Update AttendanceController.js

--- controllers/AttendanceController.js
+++ controllers/AttendanceController.js
@@ -1,5 +1,5 @@
 const Attendance = require("../models/studentAttendance");
- 
+ const Year = require("../models/Year");
 // Mark Attendance or Edit Attendance
 const markAttendance = async (req, res) => {
   try {
