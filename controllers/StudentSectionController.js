4b68a5fb69b961b82fa0401c069e2b0976ca2b2d
Sanjay-adepu <adepusanjay812@gmail.com>
2025-01-30 23:11:51 +0530

    Update StudentSectionController.js

--- controllers/StudentSectionController.js
+++ controllers/StudentSectionController.js
@@ -447,6 +447,7 @@ const getStudentByRollNumber = async (req, res) => {
         name: student.name,
         rollNumber: student.rollNumber,
         fatherName: student.fatherName || null,
+        role,
         year,
         department,
         section,
