48bce8801d3f7f0c3da525ed8fbf5f84a811e7f8
Sanjay-adepu <adepusanjay812@gmail.com>
2025-02-07 21:13:38 +0530

    Update facultymodel.js

--- models/facultymodel.js
+++ models/facultymodel.js
@@ -7,7 +7,7 @@ const TimetableSchema = new mongoose.Schema({
       periodNumber: { type: Number, required: true },
       subject: { type: String, required: true },
       year: { type: String, required: true },
-      department: { type: String, required: true }, 
+      department: { type: String, required: true },
       section: { type: String, required: true },
     },
   ],
@@ -19,9 +19,13 @@ const FacultySchema = new mongoose.Schema(
     facultyId: { type: String, unique: true, required: true },
     role: { type: String, required: true },
     department: { type: String, required: true },
-    subject: { type: String, required: true }, // Added subject field
-    designation: { type: String, required: true }, // Added designation field
-    password: { type: String, required: true }, // Store already hashed passwords
+    subject: { type: String, required: true },
+    designation: { type: String, required: true },
+    qualification: { type: String, required: true }, // New field
+    experience: { type: String, required: true }, // New field
+    areaOfInterest: { type: [String], required: true }, // New field (Array to store multiple interests)
+    jntuId: { type: String, required: true }, // New field
+    password: { type: String, required: true },
     timetable: [TimetableSchema],
     image: { type: String },
   },
