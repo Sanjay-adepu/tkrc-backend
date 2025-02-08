8157f0f2b0977bdbfaf2c9d7e367d0ce4a6a6453
Sanjay-adepu <adepusanjay812@gmail.com>
2025-02-08 23:40:17 +0530

    Update editPermission.js

--- models/editPermission.js
+++ models/editPermission.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 
 const editPermissionSchema = new mongoose.Schema({
-  facultyId: { type: String, required: true }, // Changed to String
+  facultyId: { type: String, required: true }, // Storing facultyId as String
   year: { type: String, required: true },
   department: { type: String, required: true },
   section: { type: String, required: true },
-  date: { type: String, required: true },
+  startDate: { type: String, required: true }, // Start of allowed date range
+  endDate: { type: String, required: true }, // End of allowed date range
   startTime: { type: Date, required: true },
   endTime: { type: Date, required: true },
 });
