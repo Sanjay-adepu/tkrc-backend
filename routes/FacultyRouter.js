e138e659b6007786c76d43a55116b7affd909d4f
Sanjay-adepu <adepusanjay812@gmail.com>
2025-02-10 21:30:14 +0530

    Update FacultyRouter.js

--- routes/FacultyRouter.js
+++ routes/FacultyRouter.js
@@ -3,7 +3,7 @@ const multer = require("multer")
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const cloudinary = require("../cloudnaryConfig.js");
 
-loginAdmin
+
 
 
 // Set up Cloudinary storage
